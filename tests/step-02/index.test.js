0a193eb4d2f243ed3ed31f78aca65c6c26a13a18
PayalKumari10 <kumaripayal7488@gmail.com>
2024-04-26 12:43:58 +0530

    solved case test 2

--- tests/step-02/index.test.js
+++ tests/step-02/index.test.js
@@ -2,8 +2,9 @@ const { readCSV }  = require("../../src/csvReader");
 
 test(
     "Read CSV File", async () => {
-    const data = await readCSV("./student.csv");
     
+    const data = await readCSV("./student.csv");
+
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(4);
     expect(data[0].name).toBe("John");
